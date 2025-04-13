5ba014801b50f85fa315957f68512dfbb4ffd72b
sumonmia360 <sumonmia4526@gmail.com>
2025-04-13 09:50:21 +0600

    Alhamdulillah motion features is added in skills secton, project seciton, and more

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,16 @@
 import { HERO } from "../constants";
 import sumon_img from "../assets/sumonmia.png";
+import { motion } from "framer-motion";
+
 const Hero = () => {
   return (
     <section className="flex min-h-screen items-center flex-wrap">
-      <div className="w-full md:w-1/2">
+      <motion.div
+        initial={{ opacity: 0, x: -50 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+        className="w-full md:w-1/2"
+      >
         <h2 className="my-8 text-4xl font-bold md:text-5xl lg:text-[7rem]">
           {HERO.name}
         </h2>
@@ -11,12 +18,25 @@ const Hero = () => {
           {HERO.greet}
         </p>
         <p className="mb-8 p-2 text-xl">{HERO.description}</p>
-      </div>
-      <div className="w-full md:w-1/2 lg:p-8">
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: 0.8 }}
+        animate={{ opacity: 1, x: 1 }}
+        transition={{ duration: 1 }}
+        className="w-full md:w-1/2 lg:p-8"
+      >
         <div className="flex justify-center">
-          <img src={sumon_img} alt="" width={550} height={550} />
+          <motion.img
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            src={sumon_img}
+            alt=""
+            width={550}
+            height={550}
+          />
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 };
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,28 @@
 import { SKILLS } from "../constants";
-
+import { motion, stagger } from "framer-motion";
+const containerVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 1, staggerChildren: 0.5 },
+  },
+};
+const itemVariants = {
+  hidden: { opacity: 0, x: -20 },
+  visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+};
 const Skills = () => {
   return (
     <div className="container mx-auto" id="skills">
       <h2 className="mb-12 mt-20 text-center text-4xl font-semibold">Skills</h2>
-      <div className="mx-2 flex flex-col rounded-3xl px-4 py-2 lg:px-20 border border-stone-50/30">
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        variants={containerVariants}
+        viewport={{ once: true }}
+        className="mx-2 flex flex-col rounded-3xl px-4 py-2 lg:px-20 border border-stone-50/30"
+      >
         {SKILLS.map((skills, index) => (
           <div
             key={index}
@@ -22,7 +40,7 @@ const Skills = () => {
             </div>{" "}
           </div>
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 };
